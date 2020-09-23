9141b6bc087dbeb6693c0d81e79557d32f54ba9e
Your Name <you@example.com>
2020-09-23 16:26:29 +0600

    Database Seeding

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -8,15 +8,15 @@ class Reply extends Model
 {
     public function question(){
 
-        return $this->belongsTo('Question');
+        return $this->belongsTo(Question::class);
     }
     public function user(){
 
-        return $this->belongsTo('User');
+        return $this->belongsTo(User::class);
     }
 
     public function like(){
 
-        return $this->hasMany('Like');
+        return $this->hasMany(Like::class);
     }
 }
