e699dc37f1b15110bc2d9c884a6e6b89f8f6e25d
Your Name <you@example.com>
2020-10-04 13:53:42 +0600

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,25 @@ use App\Model\Question;
 Route::apiResource('/question','QuestionController');
 Route::apiResource('/category','CategoryController');
 Route::apiResource('/question/{question}/reply','ReplyController');
+Route::post('/like/{reply}','LikeController@likeIt');
+Route::delete('/like/{reply}','LikeController@unLikeIt');
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
 
-
-Route::get('/test',function(){
-
-    $papan = "iam papan das";
-    return Str::slug($papan);
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('me', 'AuthController@me');
 });
+
+// Route::get('/test',function(){
+
+//     $papan = "iam papan das";
+//     return Str::slug($papan);
+// });
 
 
 // Route::get('/test/{id}',function($id){
